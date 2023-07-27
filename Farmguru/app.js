12282238b31164c41f9c7b39817e017c8abe90c4
Ankit Pandey <ankit.sanupandey@gmail.com>
2023-07-28 03:19:32 +0530

    Initial Commit

---
+++ Farmguru/app.js
@@ -0,0 +1,163 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'organic banana',
+        image: '1.PNG',
+        price: 60
+    },
+    {
+        id: 2,
+        name: 'organic tomato',
+        image: '2.PNG',
+        price: 12
+    },
+    {
+        id: 3,
+        name: 'organic carrot',
+        image: '3.PNG',
+        price: 22
+    },
+    {
+        id: 4,
+        name: 'natural milk',
+        image: '4.PNG',
+        price: 65
+    },
+    {
+        id: 5,
+        name: 'organic grapes',
+        image: '5.PNG',
+        price: 120
+    },
+    {
+        id: 6,
+        name: 'natural almonds',
+        image: '6.PNG',
+        price: 650
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
+
+
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 150){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+
+}
+
+let countDate = new Date('june 1, 2021 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+
+
+
---
+++ README.md
@@ -0,0 +1,27 @@
+<h3>1. Farmguru</h3>
+<p>
+    . Dated: 29 May 2023 <br>
+    . A very beautiful and attractive grocery website with almost every elements <br>
+    . the add to cart facility is provided with advance concepts of Javascript.
+</p>
+<br><br>
+<h3>2. PostWomen</h3>
+<p>
+    . Dated: 10 June 2023 <br>
+    . PostWomen is a web application designed to simplify and streamline the process of testing, developing, and documenting APIs (Application Programming Interfaces).<br>
+    . It allows developers to make API requests, view responses, and manage APIs in a user-friendly interface with help of fetchAPI.
+</p>
+<br><br>
+<h3>3. WallClock</h3>
+<p>
+    . Dated: 11 June 2023 <br>
+    . This interactive clock is a web-based application created using HTML, CSS, and JavaScript. It displays the current time in a user-friendly and visually appealing manner. <br>
+    . The clock's design is simple yet elegant, featuring a clean interface with hour, minute, and second hands that rotate smoothly to indicate the current time.
+</p>
+<br><br>
+<h3>4. Spotify Clone</h3>
+<p>
+    . Dated: 13 June 2023 <br>
+    . The Spotify clone is a web application built using HTML, CSS, and JavaScript that replicates some of the key features and functionalities of the popular music streaming service, Spotify. <br>
+    . The goal of this project is to provide users with a familiar and user-friendly interface for playing music with help of Audio library of JS.
+</p>
